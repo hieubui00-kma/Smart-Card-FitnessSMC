adfc4a159e1b2640e51d0bad69a10ff338893eec
hieubui00 <hieubui00.it@gmail.com>
2022-06-22 20:06:05 +0700

    Initialization and connect to Fitness Database

---
+++ src/com/kma/fitnesssmc/data/local/FitnessDatabase.java
@@ -0,0 +1,39 @@
+package com.kma.fitnesssmc.data.local;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.*;
+
+public class FitnessDatabase {
+    private static final String DATABASE_HOST = "localhost";
+    private static final String DATABASE_NAME = "fitness";
+    private static final String DATABASE_USER_NAME = "root";
+    private static final String DATABASE_PASSWORD = null;
+
+    private static FitnessDatabase instance = null;
+
+    private Connection connection = null;
+
+    private FitnessDatabase() {
+
+    }
+
+    public void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://" + DATABASE_HOST + "/" + DATABASE_NAME, DATABASE_USER_NAME, DATABASE_PASSWORD);
+    }
+
+    public ResultSet executeQuery(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        return statement.executeQuery(query);
+    }
+
+    public void close() throws SQLException {
+        connection.close();
+    }
+
+    public static @NotNull FitnessDatabase getInstance() {
+        return instance == null ? instance = new FitnessDatabase() : instance;
+    }
+}
